645d9b30209bdee2ef3384e34e798badba2eb804
Rameesha Khan <rameeshakhan75@gmail.com>
2023-02-10 22:28:09 +0500

    Error Handling Done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,2 +1,9 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+//COntroller
+const userController = require("../controllers/userController")
+router.route("/").post(userController.createUser)
+
+router.route("/:id").get(userController.getUser)
+module.exports = router;
